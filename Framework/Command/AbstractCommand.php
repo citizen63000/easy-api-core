0eeb08c75c853aad764dc86c114310b0241f6deb
lionel <ngconnected@gmail.com>
2024-08-26 09:51:06 +0200

    sf6.4 (wip)

--- Framework/Command/AbstractCommand.php
+++ Framework/Command/AbstractCommand.php
@@ -6,7 +6,6 @@ use Doctrine\Persistence\ManagerRegistry;
 use EasyApiCore\Util\CoreUtilsTrait;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 abstract class AbstractCommand extends Command
@@ -23,15 +22,12 @@ abstract class AbstractCommand extends Command
     /**
      * Write log with time.
      */
-    public function writeLog(OutputInterface $output, string $message, int $option = 0)
+    public function writeLog(OutputInterface $output, string $message, int $option = 0): void
     {
         $output->writeln(date('Y-m-d H:i:s', time())." {$message}", $option);
     }
 
-    /**
-     * @return object
-     */
-    protected function get(string $id, int $invalidBehavior = Container::EXCEPTION_ON_INVALID_REFERENCE)
+    protected function get(string $id, int $invalidBehavior = ContainerInterface::EXCEPTION_ON_INVALID_REFERENCE): ?object
     {
         return $this->container->get($id, $invalidBehavior);
     }
@@ -53,10 +49,7 @@ abstract class AbstractCommand extends Command
         return $this->container->get('doctrine');
     }
 
-    /**
-     * @return mixed
-     */
-    public function getParameter(string $name)
+    public function getParameter(string $name): \UnitEnum|float|array|bool|int|string|null
     {
         return $this->container->getParameter($name);
     }
